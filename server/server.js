8b697474bef728f2d90b8d4fe307d64bbec85876
ahmadizzan <aahmadizzan@gmail.com>
2017-02-04 21:18:33 +0700

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -14,6 +14,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
    console.log('New user connected');
 
+   socket.emit('newMessage', {
+      from: 'Admin',
+      text: 'Welcome to the chat app',
+      createdAt: new Date().getTime()
+   });
+
+   socket.broadcast.emit('newMessage', {
+      from: 'Admin',
+      text: 'A new user has joined',
+      createdAt: new Date().getTime()
+   });
+
    // socket.emit('newEmail', {
    //    from: 'izzan@imel.com',
    //    text: 'test ting',
